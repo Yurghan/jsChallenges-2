cf160e29cdd829a03854de6bf051f72643081589
Miłosz Topolewski <topolewski.webmilo@gmail.com>
2022-08-11 18:09:55 +0200

    e1

---
+++ 3-medium/4-task.js
@@ -0,0 +1,6 @@
+// Use a regular expression to test for an empty string.
+// const REGEXP = /your solution/
+// REGEXP.test("") ➞ true
+
+const REGEXP = /^$/;
+console.log(REGEXP.test(''));
--- temporary.js
+++ temporary.js
@@ -1,16 +1,1 @@
-function allAboutStrings(str) {
-  const result = [];
-  const length = str.length;
-
-  result.push(length);
-  result.push(str[0]);
-  result.push(str[length - 1]);
-  length % 2 ? result.push(str[(length + 1) / 2 - 1]) : result.push(str.slice(length / 2 - 1, length / 2 + 1));
-
-  const theIndex = str.split('').indexOf(str[1], 2);
-  theIndex === -1 ? result.push('not found') : result.push(`@ index ${theIndex}`);
-
-  return result;
-}
-
-console.log(allAboutStrings('AanMn'));
+const REGEXP = new RegExp();
