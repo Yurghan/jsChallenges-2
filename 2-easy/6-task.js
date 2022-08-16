3a7dd3431f967b67c60e875558fc60c528078e6e
Miłosz Topolewski <topolewski.webmilo@gmail.com>
2022-08-16 13:30:49 +0200

    ve

--- 2-easy/6-task.js
+++ 2-easy/6-task.js
@@ -1,1 +1,6 @@
 // Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+const minMax = (arr) => {
+  arr.sort((a, b) => a - b);
+  return [arr.at(0), arr.at(-1)];
+};
--- temporary.js
+++ temporary.js
@@ -1,3 +1,6 @@
-const minMax = (arr) => {};
+const minMax = (arr) => {
+  arr.sort((a, b) => a - b);
+  return [arr.at(0), arr.at(-1)];
+};
 
-console.log(minMax([1, 2, 3, 4, 5]));
+console.log(minMax([1, 2, 22, 3, 4, 5]));
