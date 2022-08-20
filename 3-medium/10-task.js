aedf858a01952cf404990ec6dbf3413824075fc1
Miłosz Topolewski <topolewski.webmilo@gmail.com>
2022-08-20 12:55:13 +0200

    m

---
+++ 3-medium/10-task.js
@@ -0,0 +1,2 @@
+// Create a function that takes numbers b and m as arguments and returns the derivative of the function f(x)=x^b with respect to x evaluated at x=m, where b and m are constants.
+// derivative(3, -2) ➞ 12
